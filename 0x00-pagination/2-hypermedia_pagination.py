6c1b8563d63286d4e13af878fa6509f0ce8e30c8
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-08-23 16:47:46 +0300

    Fixed incorrect name for the csv file

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -25,7 +25,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
 class Server:
     """Server class to paginate a database of popular baby names.
     """
-    DATA_FILE = "Popular_Baby_names.csv"
+    DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
