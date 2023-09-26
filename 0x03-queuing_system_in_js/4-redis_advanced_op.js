6408b9ad44af32e54a6e414c3c191f88331ceea5
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-09-26 16:25:56 +0300

    Node Redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,22 @@
+const redis = require('redis');
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+	console.log('Redis client connected to the server');
+})
+
+client.hset('HolbertonSchools', 'Portlands', 50, redis.print);
+client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+client.hset('HolbertonSchools', 'New York', 20, redis.print);
+client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+
+client.hgetall('HolbertonSchools', (error, reply) => {
+	if (error) {
+		console.error('Error:', error);
+	} else {
+		console.log(reply);
+	}
+});
