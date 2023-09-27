c97afb556a60b7870fc08b6666008b5c6fa2186f
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-09-27 13:57:54 +0300

    Track progress and errors with Kue: Create the job processor

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,40 @@
+const kue = require('kue');
+const queue = kue.createQueue();
+
+const blacklistedNumbers = ['4153518780', '4153518781'];
+
+function sendNotification(phoneNumber, message, job, done) {
+	let progress = 0;
+
+	const progressInterval = setInterval(() => {
+		job.progress(progress, 100);
+		progress += 10;
+
+		if (progress > 100) {
+			clearInterval(progressInterval);
+		}
+	}, 1000);
+
+	if (blacklistedNumbers.includes(phoneNumber)) {
+		const err = `Phone number ${phoneNumber} is blacklisted`;
+		done(new Error(err));
+	} else {
+		// Simulate sending a notification
+		setTimeout(() => {
+			// Stop progress tracking
+			clearInterval(progressInterval);
+			// Update progress to 50
+			job.progress(50, 100);
+			// Log the notification
+			console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+			done();
+		}, 2000);
+	}
+}
+
+
+// Process jobs from the queue with concurrency 2
+queue.process('push_notification_code_2', 2, (job, done) => {
+	const { phoneNumber, message } = job.data;
+	sendNotification(phoneNumber, message, job, done);
+});
