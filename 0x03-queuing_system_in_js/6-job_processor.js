53136af60f158c89adbc9ddf1def9c81bb029a91
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-09-27 14:59:35 +0300

    Made ES6 changes to codes for tasks 6-9

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -1,4 +1,4 @@
-const kue = require('kue');
+import kue from 'kue';
 const queue = kue.createQueue();
 
 function sendNotification(phoneNumber, message) {
--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -1,4 +1,4 @@
-const kue = require('kue');
+import kue from 'kue';
 const queue = kue.createQueue();
 
 const blacklistedNumbers = ['4153518780', '4153518781'];
